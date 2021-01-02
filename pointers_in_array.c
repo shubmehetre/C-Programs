e24db76d6359d475dd4e3e42812acba37b8da96c
Shubham A Mehetre <shub.mehetre@gmail.com>
2021-01-02 13:09:00 +0530

    added practice .c codes

--- pointers_in_array.c
+++ pointers_in_array.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+void printArr(int *);
 
 int main(void)
 {
-    int a = 10;
-    int *ptoa = &a;
     int arr1[] = {1,2,3,4,5};
     int *ptoarr1 = arr1;
 
+    printArr(ptoarr1);
+    return 0;
+}
+void printArr (int *ptoarr1)
+{
     for (int i = 0; i<5; i++)
     {
         printf("%d ", *ptoarr1);
         ptoarr1 += 1;
     }
-    // printf ("%d ", arr1);
-    // printf ("%d ", ptoarr1);
-    // printf ("%d ", arr1[0]);
-    // printf ("%d ", *(ptoarr1+4));
-    // printf ("%d ", ptoarr1+1);
-    
-    //printf("%d", *ptoa + 10);
 }
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-    int arr[] = 100;
-
-    
-}
